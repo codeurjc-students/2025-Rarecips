7c83670047380a19dd6670bdef97041553039dad
Blas Vita Ramos <b.vita.2020@alumnos.urjc.es>
2026-06-02 15:01:19 +0200

    Added healthchecks for CD workflow

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 TAG=${1:-dev-compose}
 DOCKERHUB_USER=${DOCKERHUB_USER:-blasetvrtumi}
+DEPLOYED_DOMAIN=${DEPLOYED_DOMAIN:-https://rarecips.spaincentral.cloudapp.azure.com/}
 
 echo "Deploying OCI package: ${DOCKERHUB_USER}/rarecips:${TAG}..."
 
@@ -11,6 +12,19 @@ docker pull "${DOCKERHUB_USER}/rarecips:${TAG}"
 
 # Run OCI
 docker compose -p rarecips -f "oci://docker.io/${DOCKERHUB_USER}/rarecips:${TAG}" up -y -d
+
+# Healthchecks
+echo "Waiting for the application to be healthy locally..."
+until curl -s -f -o /dev/null http://localhost:8443/; do
+  sleep 3
+done
+echo "Application is healthy locally"
+
+echo "Waiting for the application to be healthy externally..."
+until curl -s -f -o /dev/null "${DEPLOYED_DOMAIN}"; do
+  sleep 3
+done
+echo "Application is healthy externally"
 
 # Clean up
 docker system prune -f
